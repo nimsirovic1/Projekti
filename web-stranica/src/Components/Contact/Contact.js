6c4781493e4fc5debee371d921c3d585efd9562e
nimsirovic1 <nimsirovic1@etf.unsa.ba>
2019-06-17 22:08:22 +0200

    Kreirana contact me forma

--- web-stranica/src/Components/Contact/Contact.js
+++ web-stranica/src/Components/Contact/Contact.js
@@ -1,11 +1,54 @@
 import React from "react";
-
+import "./ContactStil.css";
 
 class Contact extends React.Component {
   render() {
     return (
       <div className="main-div">
-        Heej, u Contact formi siii
+        <div className="container" id="kontakt">
+          <form>          
+            <div className="row">
+              <div className="col-25"> 
+                <label htmlFor="firstNameInput" id="firstNameLabel"  className="labele">First name:*</label>
+              </div>
+              <div className="col-75">
+                  <input type="text" className="form-control" placeholder="Your first name here..." id="firstNameInput"/>
+              </div>
+            </div> 
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="LastNameInput" id="LastNameLabel" className="labele">Last name:*</label>
+              </div>  
+              <div className="col-75">
+                  <input type="text" className="form-control" id="LastNameInput"  placeholder="Your last name here..."/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="EmailInput" id="EmailLabel"  className="labele">E-mail:*</label>
+              </div>
+              <div className="col-75">
+                <input type="text" className="form-control" id="EmailInput"  placeholder="Your e-mail here..."/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="SubjectInput" id="SubjectLabel" className="labele">Subject:*</label>
+              </div>
+              <div className="col-75">
+                <input type="text" className="form-control" id="SubjectInput" placeholder="Subject here..."/>
+              </div>
+            </div>
+            <div className="row">                
+              <div className="col">           
+                <textarea className="form-control" id="MessageInput" style={{padding:"15%"}} placeholder="Please enter your message here..."></textarea>
+              </div>
+            </div>
+            <div className="row">                 
+              <button type="button" id="dugmic2" className="btn btn-outline-secondary">Send</button>
+            </div>
+         </form>
+        </div>
       </div>
     );
   }
--- web-stranica/src/header.js
+++ web-stranica/src/header.js
@@ -1,15 +1,53 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './index.css';
-import github from './github.png';
+
 
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isDesktop: false,
+          show: [false] 
+        };
+    
+        this.updatePredicate = this.updatePredicate.bind(this);
+      }
+      componentDidMount() {
+        this.updatePredicate();
+        window.addEventListener("resize", this.updatePredicate);
+      }
+    
+      componentWillUnmount() {
+        window.removeEventListener("resize", this.updatePredicate);
+      }
+    
+      updatePredicate() {
+        this.setState({ isDesktop: window.innerWidth > 600 });
+      }
+
+      
+    
+      showHide(num){
+        this.setState((prevState) => {
+            const newItems = [...prevState.show];
+            newItems[num] = !newItems[num];
+            return {show: newItems};
+        });
+      }
+
     render() {
+       
+ const isDesktop = this.state.isDesktop;
         return (
-            <div className="nav-link active"> 
-               <div id="meni">
-               <Link onClick={() => window.open( 'https://github.com/nimsirovic1/Projekti')}><img src={github} alt={"Github logo"} className="logo" /></Link>
+            
+        <div className="nav-link active"> 
+        {isDesktop ? (
+          
+          
+            <div id="meni">
+               <Link onClick={() => window.open( 'https://github.com/nimsirovic1/Projekti')} className="fa fa-github"></Link>
               <Link onClick={() => window.open( 'https://linkedin.com')} className="fa fa-linkedin"></Link>
                    <ul>
                     <li className="meni2"><Link className="link-stil" to={`/`}><i className="fa fa-fw fa-home"></i> Home </Link></li>
@@ -18,7 +56,27 @@ export default class App extends React.Component {
                     <li className="meni2"><Link className="link-stil" to={`/contact`}><i className="fa fa-fw fa-envelope"></i> Contact </Link></li>
                     </ul>
                </div>
-            </div>
-        )
+           
+            ) : (
+            <div id="mali_meni" style={{float:"right"}}>
+            
+                 
+              
+              <i className="fa fa-bars"  style={{color:"white"}} onClick={()=>this.showHide(0)}></i>
+              { this.state.show[0] && 
+                   
+                        <ul>
+                        <li className="meni2"><Link onClick={() => window.open( 'https://github.com/nimsirovic1/Projekti')} className="fa fa-github"></Link>
+                        </li>
+                        <li className="meni2" style={{marginLeft:"1%"}}><Link onClick={() => window.open( 'https://linkedin.com')} className="fa fa-linkedin"></Link></li><br />
+                        <li className="meni2"><Link className="link-stil" to={`/`}><i className="fa fa-fw fa-home"></i> Home </Link></li><br />
+                        <li className="meni2"><Link className="link-stil" to={`/about-me`}><i className="fa fa-fw fa-user"></i> About </Link></li><br />
+                        <li className="meni2"><Link className="link-stil" to={`/projects`}><i className="fa fa-folder"></i> Projects </Link></li><br />
+                        <li className="meni2"><Link className="link-stil" to={`/contact`}><i className="fa fa-fw fa-envelope"></i> Contact </Link></li>
+                        </ul>
+                  
+              }
+            </div> )} </div>
+        );
     }
 }
